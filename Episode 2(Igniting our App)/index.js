e772a74b11ffbf3eac8f696e95b39cf6d0e49f0b
Swapnil579 <singhswapnil579@gmail.com>
2024-01-26 22:33:40 +0530

    Episode 1 Done

---
+++ Episode 2(Igniting our App)/index.js
@@ -0,0 +1,9 @@
+
+let heading=React.createElement("div",{id:"Parent"},
+React.createElement("div",{id:"Child"},
+[React.createElement("h1",{},"I'm h1 tag"),React.createElement("h2",{},"I'm h2 tag")]
+));
+
+let root=ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
